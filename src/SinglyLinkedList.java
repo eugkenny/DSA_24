65722b6522800bcf7ad90b0acea84375afc89034
aileen <aileen.omara@tus.ie>
2024-10-07 16:50:27 +0100

    update linked list

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class SinglyLinkedList <E> implements List<E>{
 
     private Node<E> first;
@@ -10,17 +13,69 @@ public class SinglyLinkedList <E> implements List<E>{
 
     @Override
     public void insert(int index, E element) {
+        if(index < 0 || index > size){
+            throw new ArrayIndexOutOfBoundsException();
+        }
 
+        if (size == 0){
+            first = new Node<>(element, null);
+        }
+        else {
+            Node<E> current = first;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+
+            current.next = new Node<>(element, current.next);;
+        }
+
+        size++;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        if(index < 0 || index > size){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        Node<E> current = first;
+        for (int i = 0; i < index - 1; i++){
+            current = current.next;
+        }
+
+        E oldValue = current.next.element;
+
+        current.next = current.next.next;
+        size--;
+
+        return oldValue;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public String toString(){
+        Iterator<E> it = iterator();
+
+        if (!it.hasNext()){
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[" + it.next());
+        while (it.hasNext()){
+            sb.append(", " + it.next());
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    //@Override
+    public Iterator<E> iterator() {
+        return new ListIterator();
     }
 
     private static class Node<E>{
@@ -30,6 +85,34 @@ public class SinglyLinkedList <E> implements List<E>{
         Node(E element, Node<E> next){
             this.element = element;
             this.next = next;
+        }
+    }
+
+    private class ListIterator implements Iterator<E> {
+
+        private Node<E> current;
+
+        public ListIterator() {
+            current = first;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) throw new NoSuchElementException();
+
+            E element = current.element;
+            current = current.next;
+
+            return element;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
         }
     }
 }
